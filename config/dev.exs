1957a1a252b479eb491238ea10e8944114d2ae9b
Gaurav Gampa <gampa.gaurav@gmail.com>
2022-11-20 00:37:17 -0800

    Dockerize the application

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :ad_interview, AdInterview.Repo,
   username: "postgres",
   password: "postgres",
   database: "ad_interview_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
---
+++ scripts/init-local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\n======= \033[1;32mmix do deps.get, compile\e[0m =======\n" &&\
+  mix do deps.get, compile &&\
+echo -e "\n======= \033[1;32mmix ecto.create\e[0m =======\n" &&\
+  mix ecto.create -r AdInterview.Repo &&\
+echo -e "\n======= \033[1;32mmix ecto.load\e[0m =======\n" &&\
+  mix ecto.load -r AdInterview.Repo --quiet --skip-if-loaded &&\
+echo -e "\n======= \033[1;32mmix ecto.migrate\e[0m =======\n" &&\
+  mix ecto.migrate -r AdInterview.Repo  &&\
+echo -e "\n======= \033[1;32mmix emporos.seeds\e[0m =======\n" &&\
+  mix emporos.seeds
